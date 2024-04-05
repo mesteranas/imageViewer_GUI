c6e299bfb3030800ebdc793e03f385cf9a66cc3d
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-04-05 17:20:04 +0200

    04/05/2024

---
+++ imageViewer/main.py
@@ -0,0 +1,103 @@
+import sys
+from custome_errors import *
+sys.excepthook = my_excepthook
+import   requests
+import update
+import gui
+import guiTools
+from settings import *
+import PyQt6.QtWidgets as qt
+import PyQt6.QtGui as qt1
+import PyQt6.QtCore as qt2
+language.init_translation()
+class main (qt.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(app.name + _("version : ") + str(app.version))
+        layout=qt.QVBoxLayout()
+        self.image=        qt.QLabel()
+        layout.addWidget(self.image)
+        self.setting=qt.QPushButton(_("settings"))
+        self.setting.setDefault(True)
+        self.setting.clicked.connect(lambda: settings(self).exec())
+        layout.addWidget(self.setting)
+        w=qt.QWidget()
+        w.setLayout(layout)
+        self.setCentralWidget(w)
+
+        mb=self.menuBar()
+        open=mb.addMenu(_("open"))
+        file=qt1.QAction(_("open file"),self)
+        open.addAction(file)
+        file.triggered.connect(self.on_file)
+        url=qt1.QAction(_("open from internet"),self)
+        open.addAction(url)
+        url.triggered.connect(self.on_url)
+        help=mb.addMenu(_("help"))
+        helpFile=qt1.QAction(_("help file"),self)
+        help.addAction(helpFile)
+        helpFile.triggered.connect(lambda:guiTools.HelpFile())
+        helpFile.setShortcut("f1")
+        cus=help.addMenu(_("contact us"))
+        telegram=qt1.QAction("telegram",self)
+        cus.addAction(telegram)
+        telegram.triggered.connect(lambda:guiTools.OpenLink(self,"https://t.me/mesteranasm"))
+        telegramc=qt1.QAction(_("telegram channel"),self)
+        cus.addAction(telegramc)
+        telegramc.triggered.connect(lambda:guiTools.OpenLink(self,"https://t.me/tprogrammers"))
+        githup=qt1.QAction(_("Github"),self)
+        cus.addAction(githup)
+        githup.triggered.connect(lambda: guiTools.OpenLink(self,"https://Github.com/mesteranas"))
+        X=qt1.QAction(_("x"),self)
+        cus.addAction(X)
+        X.triggered.connect(lambda:guiTools.OpenLink(self,"https://x.com/mesteranasm"))
+        email=qt1.QAction(_("email"),self)
+        cus.addAction(email)
+        email.triggered.connect(lambda: guiTools.sendEmail("anasformohammed@gmail.com","project_type=GUI app={} version={}".format(app.name,app.version),""))
+        Github_project=qt1.QAction(_("visite project on Github"),self)
+        help.addAction(Github_project)
+        Github_project.triggered.connect(lambda:guiTools.OpenLink(self,"https://Github.com/mesteranas/{}".format(settings_handler.appName)))
+        Checkupdate=qt1.QAction(_("check for update"),self)
+        help.addAction(Checkupdate)
+        Checkupdate.triggered.connect(lambda:update.check(self))
+        licence=qt1.QAction(_("license"),self)
+        help.addAction(licence)
+        licence.triggered.connect(lambda: Licence(self))
+        donate=qt1.QAction(_("donate"),self)
+        help.addAction(donate)
+        donate.triggered.connect(lambda:guiTools.OpenLink(self,"https://www.paypal.me/AMohammed231"))
+        about=qt1.QAction(_("about"),self)
+        help.addAction(about)
+        about.triggered.connect(lambda:qt.QMessageBox.information(self,_("about"),_("{} version: {} description: {} developer: {}").format(app.name,str(app.version),app.description,app.creater)))
+        self.setMenuBar(mb)
+        if settings_handler.get("update","autoCheck")=="True":
+            update.check(self,message=False)
+    def closeEvent(self, event):
+        if settings_handler.get("g","exitDialog")=="True":
+            m=guiTools.ExitApp(self)
+            m.exec()
+            if m:
+                event.ignore()
+        else:
+            self.close()
+    def on_url(self):
+        try:
+            text,ok=qt.QInputDialog.getText(self,_("link"),_("enter link"))
+            if ok:
+                r=requests.get(text)
+                pixmap=qt1.QPixmap()
+                pixmap.loadFromData(r.content)
+                self.image.setPixmap(pixmap)
+        except:
+            qt.QMessageBox.warning(self,_("error"),_("please try later"))
+    def on_file(self):
+        file=qt.QFileDialog(self)
+        file.setAcceptMode(file.AcceptMode.AcceptOpen)
+        if file.exec()==file.DialogCode.Accepted:
+            pixmap=qt1.QPixmap(file.selectedFiles()[0])
+            self.image.setPixmap(pixmap)
+App=qt.QApplication([])
+w=main()
+w.show()
+App.setStyle('fusion')
+App.exec()
---
+++ imageViewer/settings/app.py
@@ -0,0 +1,6 @@
+name="image viewer "
+appName="imageViewer_GUI"
+appdirname="imageViewer"
+version=1.0
+description="a program make you to view images"
+creater="mesteranas"
